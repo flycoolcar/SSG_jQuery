11cc454bdc9efdda0977225619e8c4534e4362ac
flycoolcar <416832754@qq.com>
2019-01-04 09:27:40 +0800

    公司代码
    1

---
+++ js/my_plugin.js
@@ -0,0 +1,70 @@
+    /*
+    <!--
+1. 扩展jQuery的工具方法
+  $.extend(object)
+2. 扩展jQuery对象的方法
+  $.fn.extend(object)
+-->
+
+jQuery.extend({
+  min: function(a, b) { return a < b ? a : b; },
+  max: function(a, b) { return a > b ? a : b; }
+});
+   需求：
+   1. 给 $ 添加4个工具方法:
+     * min(a, b) : 返回较小的值
+     * max(c, d) : 返回较大的值
+     * leftTrim() : 去掉字符串左边的空格
+     * rightTrim() : 去掉字符串右边的空格
+   2. 给jQuery对象 添加3个功能方法:
+     * checkAll() : 全选
+     * unCheckAll() : 全不选
+     * reverseCheck() : 全反选
+   */
+    // $(function (param) {
+    //     console.log(1111);
+    //     $.extend({
+    //         min: function (a, b) {
+    //             return a < b ? a : b;
+    //         },
+    //         max: function (a, b) {
+    //             return a > b ? a : b;
+    //         },
+    //         leftTrim: function (str) {
+    //             return str.replace(/^\s+/, '');
+    //         },
+    //         rightTrim: function (str) {
+    //             return str.replace(/s+$/, '');
+    //         },
+    //     });
+
+    // })
+(function (param) {
+                    // alert(1111);
+                        $.extend({
+                            min: function (a, b) {
+                                return a < b ? a : b;
+                            },
+                            max: function (a, b) {
+                                return a > b ? a : b;
+                            },
+                            leftTrim: function (str) {
+                                return str.replace(/^\s+/, '');
+                            },
+                            rightTrim: function (str) {
+                                return str.replace(/\s+$/, '');
+                            },
+                            
+                        });
+                        $.fn.extend({
+                            check: function() {
+                              return this.each(function() { this.checked = true; });
+                            },
+                            uncheck: function() {
+                              return this.each(function() { this.checked = false; });
+                            },
+                            reverse: function() {
+                              return this.each(function() { this.checked = !this.checked; });
+                            },
+                          });
+                  })()

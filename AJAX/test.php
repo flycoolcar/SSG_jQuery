7e1b653b197ed500ca3c9670504da2f38b9501a4
WangJian <416832754@qq.com>
2019-01-05 08:13:41 +0800

    Create test.php

---
+++ AJAX/test.php
@@ -0,0 +1,13 @@
+<?php
+header('content-type: text/html; charset=utf-8');
+echo ('这个是测试！！！');
+$aa = 10;
+echo ($aa);
+echo('<br>');
+if ($aa > 3) {
+    echo (1111);
+} else {
+    # code...
+}
+
+?>

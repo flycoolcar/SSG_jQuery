78c67c28dddebbbba06d5c0be536f85a7f63d61a
WangJian <416832754@qq.com>
2019-01-15 21:26:12 +0800

    Create 删除-db.js

---
+++ sgg_node_js/mongo/删除-db.js
@@ -0,0 +1,14 @@
+use xyj
+db.xyj.find()
+// db.inventory.deleteMany({})
+// 删除 status 等于 A 的全部文档：
+/*db.inventory.deleteMany({ status : "A" })
+删除 status 等于 D 的一个文档：
+
+db.inventory.deleteOne( { status: "D" } )
+*/
+// show collections
+db.xyj.deleteMany({'name':'小妖精4'})
+
+
+

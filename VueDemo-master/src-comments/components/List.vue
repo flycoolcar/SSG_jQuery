9a290f2dde8e9062a2fb6f98d86c9427caebb2f0
flycoolcar <416832754@qq.com>
2019-01-30 16:49:35 +0800

    vue

---
+++ VueDemo-master/src-comments/components/List.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="col-md-8">
+    <h3 class="reply">评论回复：</h3>
+    <h2 v-show="comments.length===0">暂无评论，点击左侧添加评论吧！</h2>
+    <ul class="list-group">
+      <Item v-for="(comment, index) in comments" :key="index" :comment="comment" :deleteComment="deleteComment" :index="index"></Item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Item from './Item.vue'
+export default {
+  props: {
+    comments: Array,
+    deleteComment: Function
+  },
+
+  components: {
+    Item
+  }
+}
+</script>
+
+<style scoped>
+  .reply {
+    margin-top: 0px;
+  }
+
+  li {
+    transition: .5s;
+    overflow: hidden;
+  }
+
+  .handle a {
+    display: block;
+    text-decoration: none;
+  }
+
+</style>
---
+++ VueDemo-master/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <p>click {{count}} times, count is {{eventOrOdd}}</p>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementIfOdd">increment if odd</button>
+    <button @click="incrementAsync">increment async</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      count: 0
+    }
+  },
+
+  computed: {
+    eventOrOdd () {
+      return this.count % 2 === 0 ? '偶数' : '奇数'
+    }
+  },
+
+  methods: {
+    // 增加
+    increment () {
+      const count = this.count
+      this.count = count + 1
+    },
+    decrement () {
+      const count = this.count
+      this.count = count - 1
+    },
+    incrementIfOdd () {
+      const count = this.count
+      if (count % 2 === 1) {
+        this.count = count + 1
+      }
+    },
+    incrementAsync () {
+      setTimeout(() => {
+        const count = this.count
+        this.count = count + 1
+      }, 500)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
